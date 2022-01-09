ae4aa51dd0f52a0d45376792c05aa558601d4a89
Oliver Browning <lemenade3@hotmail.com>
2022-01-09 21:54:47 +0000

    Initial CSS styling complete and grid resizing implemented

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Etch-a-Sketch
 Etch-a-Sketch style project to demonstrate skills learned in HTML, CSS and JavaScript so far
+
+Basics of grid and drawing implemented.
+
+Features to be implemented:
+- Options for Pen Colour
+- Rainbow Pen
+- Pencil (increments black gradient with each pass)
+- Eraser
+- Click on grid to draw (Allows drawing to start from centre)
+- Pop up when clean me button pressed to change grid density
+- Range slider on page to change grid density
+- CSS styling
--- script.js
+++ script.js
@@ -1,22 +1,74 @@
-const container = document.querySelector("#container");
+// Grid Creation
+
+const container = document.querySelector(".container");
+let size = 16;
 
 function makeGrid() {
-    for (let i = 0; i < 16; i++) {
+    for (let i = 0; i < size; i++) {
         const row = document.createElement('div');
-        row.setAttribute('id', 'row');
-        for (let i = 0; i < 16; i++) {
+        row.setAttribute('class', 'row');
+        for (let i = 0; i < size; i++) {
             const pixel = document.createElement('div');
-            pixel.setAttribute('id', 'pixel');
+            pixel.setAttribute('class', 'pixel');
             row.append(pixel);
             pixel.addEventListener('mouseover', function () {
                 pixel.style.background = 'black'
             });
-        }
+        };
         container.append(row);
-    }
-}
+    };
+};
 
 makeGrid();
 
+// Grid sizing
 
+let slider = document.getElementById("myRange");
+let output = document.getElementById("num");
+output.textContent = slider.value; // Display the default slider value
 
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+  output.textContent = this.value;
+  resizeGrid();
+}
+
+function resizeGrid() {
+    while (container.firstChild) container.removeChild(container.firstChild);
+    size = slider.value;
+    makeGrid();
+}
+
+// Access pixels
+
+const pixels = document.getElementsByClassName("pixel");
+
+// Button for clearing grid
+
+const clearButton = document.querySelector("#clearButton");
+
+function clearGrid() {
+    for (i = 0; i < pixels.length; i++) {
+        pixels[i].style.background = 'white';
+    };
+};
+
+clearButton.addEventListener('click', function () {
+    clearGrid();
+    modal.style.display = "block";
+});
+
+// Modal for clean board and grid selector
+
+let modal = document.getElementById("newBoardModal");
+let span = document.getElementsByClassName("close")[0];
+
+span.onclick = function closeModal() {
+  modal.style.display = "none";
+}
+
+window.onclick = function exitModal(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
